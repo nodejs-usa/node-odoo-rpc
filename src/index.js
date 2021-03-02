e069ee21158ca692d8a0c63292cb631304ad774f
Marlom <falconsoft.3d@gmail.com>
2021-03-02 18:44:38 +0100

    sc

---
+++ src/index.js
@@ -0,0 +1,25 @@
+var Odoo = require('odoo-xmlrpc');
+
+var odoo = new Odoo({
+    url: '127.0.0.1',
+    port: 8069,
+    db: 'db14-spain',
+    username: 'admin',
+    password: 'x1234567890'
+});
+
+odoo.connect(function (err) {
+    if (err) { return console.log(err); }
+    console.log('Connected to Odoo server.');
+    var inParams = [];
+    inParams.push([['active', '=', true]]);
+    var params = [];
+    params.push(inParams);
+
+    odoo.execute_kw('res.partner', 'search', params, function (err, value) {
+        if (err) { return console.log(err); }
+        value.forEach(element => console.log(element));
+        console.log('Result: ', value);
+    });
+    
+});
