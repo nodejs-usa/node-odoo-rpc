eb4c2664a646d22774a9a701870c7e790d01ce84
Marlom <falconsoft.3d@gmail.com>
2021-03-02 19:01:35 +0100

    scc

--- src/index.js
+++ src/index.js
@@ -28,5 +28,35 @@ odoo.connect(function (err) {
         value.forEach(element => console.log(element));
         console.log('Result: ', value);
     });
+
+    // 2- Count
+    odoo.execute_kw('res.partner', 'search_count', params, function (err, value) {
+        if (err) { return console.log(err); }
+        console.log('Result: ', value);
+    });
+
+    // 4- Read
+    odoo.execute_kw('res.partner', 'search', params, function (err, value) {
+        if (err) { return console.log(err); }
+        var inParams = [];
+        inParams.push(value); //ids
+        inParams.push(['name', 'website', 'email', 'lang']);
+        var params = [];
+        params.push(inParams);
+        
+        odoo.execute_kw('res.partner', 'read', params, function (err2, value2) {
+            if (err2) { return console.log(err2); }
+            console.log('Result: ', value2);
+        });
+    });
+
+    var inParams = [];
+    inParams.push({'name': 'Marlon'})
+    var params = [];
+    params.push(inParams);
+    odoo.execute_kw('res.partner', 'create', params, function (err, value) {
+        if (err) { return console.log(err); }
+        console.log('Result: ', value);
+    });
     
 });
